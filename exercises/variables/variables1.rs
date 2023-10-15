213b518e083a1346a2fcc43cb9f2a741e99d8e8a
ArtLiathain <artp.oliathain@gmail.com>
2023-10-15 14:39:52 +0100

    Online repo for rustlings

---
+++ exercises/variables/variables1.rs
@@ -0,0 +1,13 @@
+// variables1.rs
+//
+// Make me compile!
+//
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    let x = 5;
+    let y = 7;
+    println!("x has the value {} {}", x, y);
+}
