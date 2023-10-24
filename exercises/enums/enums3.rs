05d9c8668d54bc9dcfc4af70f0717f9918568176
ArtLiathain <artp.oliathain@gmail.com>
2023-10-24 09:48:04 +0100

    Finish Enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8,u8,u8),
+    Quit,
+    Echo(String),
+    Move(Point)
 }
 
 struct Point {
@@ -45,6 +49,21 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message
+        {
+            Message::ChangeColor(r,g,b) => {
+                self.change_color((r,g,b));
+            },
+            Message::Quit => {
+                self.quit();
+            },
+            Message::Echo(word) => {
+                self.echo(word);
+            }
+            Message::Move(point) => {
+                self.move_position(point);
+            }
+        }
     }
 }
 
