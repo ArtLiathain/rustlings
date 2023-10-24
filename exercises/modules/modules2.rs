87201edbfae44cae566f0846f979a51b7e08885d
ArtLiathain <artp.oliathain@gmail.com>
2023-10-24 12:10:28 +0100

    Modules2

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -11,8 +11,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    use self::fruits::PEAR as fruit;
+    use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -24,7 +24,6 @@ mod delicious_snacks {
         pub const CARROT: &'static str = "Carrot";
     }
 }
-
 fn main() {
     println!(
         "favorite snacks: {} and {}",
